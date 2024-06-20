19cb849030517ff6605afe2437cb6af4c291cfbd
oksuzd <okssuzdal@gmail.com>
2024-06-20 09:17:56 -0300

    task with basic forms added

---
+++ src/app/basic-forms/models/newUser.model.ts
@@ -0,0 +1,12 @@
+export interface NewUser {
+  name: string;
+  surname: string;
+  lang: string;
+  role: string;
+  skills: string[];
+}
+
+export interface Configs {
+  ru: string;
+  en: string;
+}
